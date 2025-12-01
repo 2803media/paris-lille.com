dbe828745ad652019ff5f1eb3991fd7aa45856c4
2803media <hlabarre@gmail.com>
2025-12-01 20:26:15 +0100

    update design

---
+++ data.js
@@ -0,0 +1,323 @@
+// Fichier: data.js
+const galleryData = [
+  {
+    title: "La mini",
+    full: "images/full/la-mini.webp",
+    thumb: "images/thumb/la-mini.webp",
+  },
+  {
+    title: "Jupette en dentelle",
+    full: "images/full/jupette-en-dentelle.webp",
+    thumb: "images/thumb/jupette-en-dentelle.webp",
+  },
+  {
+    title: "La robette rayée",
+    full: "images/full/la-robette-rayee.webp",
+    thumb: "images/thumb/la-robette-rayee.webp",
+  },
+  {
+    title: "La blouse anti-loose",
+    full: "images/full/la-blouse-anti-loose.webp",
+    thumb: "images/thumb/la-blouse-anti-loose.webp",
+  },
+  {
+    title: "Le pluvieux",
+    full: "images/full/le-pluvieux.webp",
+    thumb: "images/thumb/le-pluvieux.webp",
+  },
+  {
+    title: "La chemise Monsieur Madame",
+    full: "images/full/la-chemise-monsieur-madame.webp",
+    thumb: "images/thumb/la-chemise-monsieur-madame.webp",
+  },
+  {
+    title: "Le bandana rayé",
+    full: "images/full/le-bandana-raye.webp",
+    thumb: "images/thumb/le-bandana-raye.webp",
+  },
+  {
+    title: "Le bandana léo",
+    full: "images/full/le-bandana-leo.webp",
+    thumb: "images/thumb/le-bandana-leo.webp",
+  },
+  {
+    title: "Le sweat qui a la frite gris",
+    full: "images/full/le-sweat-qui-a-la-frite-gris.webp",
+    thumb: "images/thumb/le-sweat-qui-a-la-frite-gris.webp",
+  },
+  {
+    title: "Tshirt Preum's",
+    full: "images/full/tshirt-preums.webp",
+    thumb: "images/thumb/tshirt-preums.webp",
+  },
+  {
+    title: "La hot culotte",
+    full: "images/full/la-hot-culotte.webp",
+    thumb: "images/thumb/la-hot-culotte.webp",
+  },
+  {
+    title: "Taies rayées",
+    full: "images/full/taies-rayees.webp",
+    thumb: "images/thumb/taies-rayees.webp",
+  },
+  {
+    title: "Taies léo",
+    full: "images/full/taies-leo.webp",
+    thumb: "images/thumb/taies-leo.webp",
+  },
+  {
+    title: "Blouse claudine Léo",
+    full: "images/full/blouse-claudine-leo.webp",
+    thumb: "images/thumb/blouse-claudine-leo.webp",
+  },
+  {
+    title: "Robette en broderie anglaise",
+    full: "images/full/robette-en-broderie-anglaise.webp",
+    thumb: "images/thumb/robette-en-broderie-anglaise.webp",
+  },
+  {
+    title: "Robette en velour rose",
+    full: "images/full/robette-en-velour-rose.webp",
+    thumb: "images/thumb/robette-en-velour-rose.webp",
+  },
+  {
+    title: "Robette en velour vert",
+    full: "images/full/robette-en-velour-vert.webp",
+    thumb: "images/thumb/robette-en-velour-vert.webp",
+  },
+  {
+    title: "Blouse anti-loose carreaux",
+    full: "images/full/blouse-anti-loose-carreaux.webp",
+    thumb: "images/thumb/blouse-anti-loose-carreaux.webp",
+  },
+  {
+    title: "Blouse anti-loose liberty",
+    full: "images/full/blouse-anti-loose-liberty.webp",
+    thumb: "images/thumb/blouse-anti-loose-liberty.webp",
+  },
+  {
+    title: "Veste froufrou",
+    full: "images/full/veste-froufrou.webp",
+    thumb: "images/thumb/veste-froufrou.webp",
+  },
+  {
+    title: "Le frileux",
+    full: "images/full/le-frileux.webp",
+    thumb: "images/thumb/le-frileux.webp",
+  },
+  {
+    title: "Le sweat qui a la frite marine",
+    full: "images/full/le-sweat-qui-a-la-frite-marine.webp",
+    thumb: "images/thumb/le-sweat-qui-a-la-frite-marine.webp",
+  },
+  {
+    title: "La robe libérée libérée délivrée",
+    full: "images/full/la-robe-liberee-liberee-delivree.webp",
+    thumb: "images/thumb/la-robe-liberee-liberee-delivree.webp",
+  },
+  {
+    title: "La jupe en jean fiz",
+    full: "images/full/la-jupe-en-jean-fiz.webp",
+    thumb: "images/thumb/la-jupe-en-jean-fiz.webp",
+  },
+  {
+    title: "La blouse claudine à carreaux",
+    full: "images/full/la-blouse-claudine-a-carreaux.webp",
+    thumb: "images/thumb/la-blouse-claudine-a-carreaux.webp",
+  },
+  {
+    title: "Le blazer Prosper",
+    full: "images/full/le-blazer-prosper.webp",
+    thumb: "images/thumb/le-blazer-prosper.webp",
+  },
+  {
+    title: "La combi Paris-Lille",
+    full: "images/full/la-combi-paris-lille.webp",
+    thumb: "images/thumb/la-combi-paris-lille.webp",
+  },
+  {
+    title: "Le top Noeud Noeud",
+    full: "images/full/le-top-noeud-noeud.webp",
+    thumb: "images/thumb/le-top-noeud-noeud.webp",
+  },
+  {
+    title: "La robe Noeud Noeud",
+    full: "images/full/la-robe-noeud-noeud.webp",
+    thumb: "images/thumb/la-robe-noeud-noeud.webp",
+  },
+  {
+    title: "Le Tshirt basique blanc",
+    full: "images/full/le-tshirt-basique-blanc.webp",
+    thumb: "images/thumb/le-tshirt-basique-blanc.webp",
+  },
+  {
+    title: "Les chemises madame petits carreaux",
+    full: "images/full/les-chemises-madame-petits-carreaux.webp",
+    thumb: "images/thumb/les-chemises-madame-petits-carreaux.webp",
+  },
+  {
+    title: "Les chemises madame grands carreaux",
+    full: "images/full/les-chemises-madame-grands-carreaux.webp",
+    thumb: "images/thumb/les-chemises-madame-grands-carreaux.webp",
+  },
+  {
+    title: "Les chemises madame lurex",
+    full: "images/full/les-chemises-madame-lurex.webp",
+    thumb: "images/thumb/les-chemises-madame-lurex.webp",
+  },
+  {
+    title: "Blazer Prince de Galles",
+    full: "images/full/blazer-prince-de-galles.webp",
+    thumb: "images/thumb/blazer-prince-de-galles.webp",
+  },
+  {
+    title: "La robette en jean",
+    full: "images/full/la-robette-en-jean.webp",
+    thumb: "images/thumb/la-robette-en-jean.webp",
+  },
+  {
+    title: "Le pluvieux numéro 2",
+    full: "images/full/le-pluvieux-numero-2.webp",
+    thumb: "images/thumb/le-pluvieux-numero-2.webp",
+  },
+  {
+    title: "La blouse frida",
+    full: "images/full/la-blouse-frida.webp",
+    thumb: "images/thumb/la-blouse-frida.webp",
+  },
+  {
+    title: "Le manteau Daddy",
+    full: "images/full/le-manteau-daddy.webp",
+    thumb: "images/thumb/le-manteau-daddy.webp",
+  },
+  {
+    title: "Le bonnet René",
+    full: "images/full/le-bonnet-rene.webp",
+    thumb: "images/thumb/le-bonnet-rene.webp",
+  },
+  {
+    title: "La maille canaille",
+    full: "images/full/la-maille-canaille.webp",
+    thumb: "images/thumb/la-maille-canaille.webp",
+  },
+  {
+    title: "La jupe Nauzan",
+    full: "images/full/la-jupe-nauzan.webp",
+    thumb: "images/thumb/la-jupe-nauzan.webp",
+  },
+  {
+    title: "La blouse Claudette à rayures",
+    full: "images/full/la-blouse-claudette-a-rayures.webp",
+    thumb: "images/thumb/la-blouse-claudette-a-rayures.webp",
+  },
+  {
+    title: "La blouse Claudette en broderie anglaise",
+    full: "images/full/la-blouse-claudette-en-broderie-anglaise.webp",
+    thumb: "images/thumb/la-blouse-claudette-en-broderie-anglaise.webp",
+  },
+  {
+    title: "La chemise Madame rayée jaune",
+    full: "images/full/la-chemise-madame-rayee-jaune.webp",
+    thumb: "images/thumb/la-chemise-madame-rayee-jaune.webp",
+  },
+  {
+    title: "Le short Ponta",
+    full: "images/full/le-short-ponta.webp",
+    thumb: "images/thumb/le-short-ponta.webp",
+  },
+  {
+    title: "Le jupon Ninon",
+    full: "images/full/le-jupon-ninon.webp",
+    thumb: "images/thumb/le-jupon-ninon.webp",
+  },
+  {
+    title: "Le blazer écru",
+    full: "images/full/le-blazer-ecru.webp",
+    thumb: "images/thumb/le-blazer-ecru.webp",
+  },
+  {
+    title: "Le basique gris",
+    full: "images/full/le-basique-gris.webp",
+    thumb: "images/thumb/le-basique-gris.webp",
+  },
+  {
+    title: "Le marcel blanc",
+    full: "images/full/le-marcel-blanc.webp",
+    thumb: "images/thumb/le-marcel-blanc.webp",
+  },
+  {
+    title: "Le sweat pépite",
+    full: "images/full/le-sweat-pepite.webp",
+    thumb: "images/thumb/le-sweat-pepite.webp",
+  },
+  {
+    title: "La robe Sarah vichy noir",
+    full: "images/full/la-robe-sarah-vichy-noir.webp",
+    thumb: "images/thumb/la-robe-sarah-vichy-noir.webp",
+  },
+  {
+    title: "La blouse Violette vichy noir",
+    full: "images/full/la-blouse-violette-vichy-noir.webp",
+    thumb: "images/thumb/la-blouse-violette-vichy-noir.webp",
+  },
+  {
+    title: "La robe louison",
+    full: "images/full/la-robe-louison.webp",
+    thumb: "images/thumb/la-robe-louison.webp",
+  },
+  {
+    title: "Le marcel marine",
+    full: "images/full/le-marcel-marine.webp",
+    thumb: "images/thumb/le-marcel-marine.webp",
+  },
+  {
+    title: "La robe Sarah Osaka",
+    full: "images/full/la-robe-sarah-osaka.webp",
+    thumb: "images/thumb/la-robe-sarah-osaka.webp",
+  },
+  {
+    title: "La robe Sarah vichy tangerine",
+    full: "images/full/la-robe-sarah-vichy-tangerine.webp",
+    thumb: "images/thumb/la-robe-sarah-vichy-tangerine.webp",
+  },
+  {
+    title: "La blouse Pauline Bonbon",
+    full: "images/full/la-blouse-pauline-bonbon.webp",
+    thumb: "images/thumb/la-blouse-pauline-bonbon.webp",
+  },
+  {
+    title: "Le cabas Paula",
+    full: "images/full/le-cabas-paula.webp",
+    thumb: "images/thumb/le-cabas-paula.webp",
+  },
+  {
+    title: "La jupe Sharleen",
+    full: "images/full/la-jupe-sharleen.webp",
+    thumb: "images/thumb/la-jupe-sharleen.webp",
+  },
+  {
+    title: "Le basique noir",
+    full: "images/full/le-basique-noir.webp",
+    thumb: "images/thumb/le-basique-noir.webp",
+  },
+  {
+    title: "La jupe BB vichy rouge",
+    full: "images/full/la-jupe-bb-vichy-rouge.webp",
+    thumb: "images/thumb/la-jupe-bb-vichy-rouge.webp",
+  },
+  {
+    title: "La jupe Bohème Porto",
+    full: "images/full/la-jupe-boheme-porto.webp",
+    thumb: "images/thumb/la-jupe-boheme-porto.webp",
+  },
+  {
+    title: "Le Tshirt Malo",
+    full: "images/full/le-tshirt-malo.webp",
+    thumb: "images/thumb/le-tshirt-malo.webp",
+  },
+  {
+    title: "La robe Portofino",
+    full: "images/full/la-robe-portofino.webp",
+    thumb: "images/thumb/la-robe-portofino.webp",
+  },
+];
